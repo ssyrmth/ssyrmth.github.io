4126c83b710e7d1bb6b860b22c9d3bed58c2fb47
ssyrmth <57507713+ssyrmth@users.noreply.github.com>
2021-10-23 01:05:55 -0400

    updated card transition values | cleaned up code

--- js/index.js
+++ js/index.js
@@ -26,8 +26,8 @@ Vue.component('card', {
     height: 0,
     mouseX: 0,
     mouseY: 0,
-    mouseLeaveDelay: null
-  }),
+    mouseLeaveDelay: null }),
+
   computed: {
     mousePX() {
       return this.mouseX / this.width;
@@ -39,39 +39,38 @@ Vue.component('card', {
       const rX = this.mousePX * 30;
       const rY = this.mousePY * -30;
       return {
-        transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
-      };
+        transform: `rotateY(${rX}deg) rotateX(${rY}deg)` };
+
     },
     cardBgTransform() {
       const tX = this.mousePX * -40;
       const tY = this.mousePY * -40;
       return {
-        transform: `translateX(${tX}px) translateY(${tY}px)`
-      }
+        transform: `translateX(${tX}px) translateY(${tY}px)` };
+
     },
     cardBgImage() {
       return {
-        backgroundImage: `url(${this.dataImage})`
-      }
-    }
-  },
+        backgroundImage: `url(${this.dataImage})` };
+
+    } },
+
   methods: {
     handleMouseMove(e) {
-      this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width/2;
-      this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height/2;
+      this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width / 2;
+      this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height / 2;
     },
     handleMouseEnter() {
       clearTimeout(this.mouseLeaveDelay);
     },
     handleMouseLeave() {
-      this.mouseLeaveDelay = setTimeout(()=>{
+      this.mouseLeaveDelay = setTimeout(() => {
         this.mouseX = 0;
         this.mouseY = 0;
       }, 1000);
-    }
-  }
-});
+    } } });
+
+
 
 const app = new Vue({
-  el: '#app'
-});
+  el: '#app' });
